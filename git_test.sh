0884e46baa6dfa6181816482e050415a7aa3a6e7
shishimaruwo <shishimaruwo@gmail.com>
2012-06-26 15:02:59 +0900

    2nd commit

--- git_test.sh
+++ git_test.sh
@@ -6,3 +6,6 @@
   git remote add origin ssh://git@ssh.github.com:443/shishimaruwo/test.git
   git push -u origin master
       
+  git clone ssh://git@ssh.github.com:443/shishimaruwo/test.git
+  git push origin master
+
